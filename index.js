4204525bc4fe869e91185b5cbc3bb989f9dcfe7d
Samantha Genevay <samanthagenevay@gmail.com>
2023-01-31 20:47:28 -0600

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+
+const currentUser = "Sam Genevay";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+console.log(welcomeMessage);
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Using bracket notation
+// const shortGreeting = `Welcome, ${currentUser[0]}!`;
+
+// Using slice
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
